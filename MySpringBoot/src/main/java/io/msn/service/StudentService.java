25a51a46aa671027476b3a1bbaab9059fc1087aa
manjunathln <33506624+manjunathln@users.noreply.github.com>
2019-06-22 23:14:49 +0530

    Removed Readme

--- MySpringBoot/src/main/java/io/msn/service/StudentService.java
+++ MySpringBoot/src/main/java/io/msn/service/StudentService.java
@@ -1,9 +1,14 @@
 package io.msn.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.msn.model.Student;
@@ -11,15 +16,23 @@ import io.msn.model.Student;
 @RestController
 public class StudentService {
 
-	List<Student> stdinfo=Arrays.asList(
+	List<Student> stdinfo= new ArrayList(Arrays.asList(
 		new Student(1,"manju","1"),new Student(2,"manju","2")
 		
-	);
+	));
 	
 	
-	@RequestMapping("/hello")
+	@RequestMapping("/info")
 	public List<Student> getName() {
 		return stdinfo;
 	}
 	
+
+	@RequestMapping(method=RequestMethod.POST,value="/add")
+	public String addStudent(@RequestBody Student st) {
+		stdinfo.add(st);
+		return "Added";
+	}
+	
+
 }
